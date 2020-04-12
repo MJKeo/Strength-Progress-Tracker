f88578a59d4fa53ad2b6816048c2e1ea77207399
Mike Keohane <mkeohane@gatech.edu>
2020-04-12 19:24:20 -0400

    Initial Commit

---
+++ Strength Progress Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Strength Progress Tracker
+//
+//  Created by Michael Keohane on 4/12/20.
+//  Copyright © 2020 Michael Keohne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
